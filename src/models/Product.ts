6caa7eaada76f73358aaced3b953e0a371a43a5d
Emmanuel Igwesi <igwesiemmanuel4@gmail.com>
2023-07-26 07:59:37 -0700

    working on product endpoint

---
+++ src/models/Product.ts
@@ -0,0 +1,53 @@
+import { model, Schema, Document } from "mongoose";
+
+const productSchema = new Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: "User",
+        },
+        name: {
+            type: String,
+            required: [true, "Please add a name"],
+            trim: true,
+        },
+        sku: {
+            type: String,
+            required: true,
+            default: "SKU",
+            trim: true,
+        },
+        category: {
+            type: String,
+            required: [true, "Please add a category"],
+            trim: true,
+        },
+        quantity: {
+            type: String,
+            required: [true, "Please add a quantity"],
+            trim: true,
+        },
+        price: {
+            type: String,
+            required: [true, "Please add a price"],
+            trim: true,
+        },
+        description: {
+            type: String,
+            required: [true, "Please add a description"],
+            trim: true,
+        },
+        image: {
+            type: Object,
+            default: {},
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+const Product = model("Product", productSchema);
+
+export default Product;
