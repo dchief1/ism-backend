7766530410f84a7658b865f0ef5d289804510aa9
dchief <igwesiemmanuel4@gmail.com>
2023-09-02 13:31:24 +0100

    product controller for creating a product is done

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,10 +1,42 @@
 import expressAsyncHandler from "express-async-handler";
 import { CustomRequest } from "../utils/extendRequest";
 import { Response } from "express";
+import Product from "../models/Product";
 
-export default class ProductController  {
-    // Create Product
-    createProduct = expressAsyncHandler (async (req: CustomRequest, res: Response) => {
-        res.send("Product Created")
-    });
+export default class ProductController {
+    // Create Product function
+    createProduct = expressAsyncHandler(
+        async (req: CustomRequest, res: Response) => {
+            const { name, sku, category, quantity, price, description } =
+                req.body;
+
+            // Validation
+            if (
+                !name ||
+                !sku ||
+                !category ||
+                !quantity ||
+                !price ||
+                !description
+            ) {
+                res.status(400);
+                throw new Error("Please fill in all fields.");
+            }
+
+            // Manage Image upload
+
+            // Create product
+            const product = await Product.create({
+                user: req.user.id,
+                name,
+                sku,
+                category,
+                quantity,
+                price,
+                description,
+            });
+
+            res.status(201).json(product);
+        },
+    );
 }
